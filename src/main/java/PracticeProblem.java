15fc4a2edf75c276711d90e8ad58637d234e3c52
jamin-xie-ics4u1-02-25 <Jamin.xie@student.tdsb.on.ca>
2025-03-30 03:18:08 +0000

    Update Change

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,36 @@ public class PracticeProblem {
 
 	}
 
+	public static int find(int[] array, int number) {
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == number) {
+				return i;
+			}
+		}
+		return -1;
+	}
 	
+	public static int findLast(String[] array, String string) {
+		for (int i = array.length - 1; i >= 0; i--) {
+			if (array[i].equals(string)) {
+				return i;
+			}
+		}
+		return -1;
+	}
 
+	public static int findSecond(char[] array, char character) {
+		int counter = -1;
+		
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == character) {
+				if (counter == -1) {
+					counter = i;
+				} else {
+					return i;
+				}
+			}
+		}
+		return counter;
+	}
 }
